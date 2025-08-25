1b0576c7107bf1375544fabb42383a34f627897c
yousufqadri <s.yousuf@live.com>
2025-08-25 19:07:54 +0500

    configure image domain

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'vlvqauzukigsmcepdlpy.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'deoophjehfjbmwndsvtl.supabase.co',
+      },
     ],
   },
   /* config options here */
